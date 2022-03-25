3d72b51454eca1152fbdacb65765f1c38e4bccd3
toimc <brian@toimc.com>
2022-03-25 19:52:57 +0800

    fix: menu高度修正

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -1,6 +1,8 @@
 <template>
   <!-- menu -->
   <el-menu
+    v-bind="$attrs"
+    ref="menuRef"
     :default-active="'1'"
     class="el-menu-custom flex-1"
     :mode="mode"
@@ -9,10 +11,9 @@
     text-color="#ffffffb3"
     active-text-color="#fff"
     :style="{ '--menu-width': width }"
-    v-bind="$attrs"
   >
     <slot></slot>
-    <el-scrollbar :height="menuHeight">
+    <el-scrollbar ref="scroll" :max-height="menuHeight">
       <template v-for="item in menusWithKeys" :key="item.path">
         <sub-menu :item="item"></sub-menu>
       </template>
@@ -55,25 +56,42 @@
       const { menus } = toRefs(props)
       // 设置menu的调试
       const menuHeight = ref(0)
+      const scroll = ref()
+      const menuRef = ref()
       const { genMenuKeys } = useNav(ctx.emit)
 
       // 给树形菜单添加key
       const menusWithKeys = genMenuKeys(menus.value)
-      onMounted(() => {
-        // 获取slots的调试
+
+      const initHeight = async () => {
         const { slots } = ctx
         if (slots.default) {
           const defaults = slots.default()
           if (defaults.length) {
             const elRef = defaults[0]
             const dom = elRef.el as HTMLElement
+            // -100的目的是距离底部一定的距离
             menuHeight.value = window.innerHeight - dom.offsetHeight
+            scroll.value?.update()
           }
         }
+      }
+
+      onMounted(() => {
+        // 获取slots的高度
+        initHeight()
+
+        const debouncedFn = useDebounceFn(() => {
+          initHeight()
+        }, 500)
+
+        useResizeObserver(menuRef, debouncedFn)
       })
+
       return {
         menusWithKeys,
-        menuHeight
+        menuHeight,
+        menuRef
       }
     }
   })
