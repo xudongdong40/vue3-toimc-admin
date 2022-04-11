0b0c59b5070d13842399c03c8aebb1f8845e5e87
toimc <brian@toimc.com>
2022-04-11 11:10:22 +0800

    feat: VueEcharts集成

--- src/components/DEcharts/index.vue
+++ src/components/DEcharts/index.vue
@@ -4,8 +4,7 @@
 
 <script lang="ts">
   import * as echarts from 'echarts'
-  import { debounce } from 'lodash-es'
-  import type { ECharts } from 'echarts'
+  import { useCharts } from './useCharts'
 
   export default defineComponent({
     props: {
@@ -19,36 +18,14 @@
       }
     },
     setup(props: any) {
-      const chartRef = ref<any>(null)
-      const chart = ref<ECharts | null>(null)
-
-      const init = (option: any) => {
-        chart.value = echarts.init(chartRef.value)
-        chart.value.setOption(option)
-
-        if (props.responsive) {
-          window.addEventListener('resize', debounce(resize, 200))
-        }
-      }
-
-      const resize = () => {
-        chart.value?.resize()
-      }
-
-      const destroy = () => {
-        chart.value = null
-        if (props.responsive) {
-          window.removeEventListener('resize', resize)
-        }
-      }
+      const { chart, chartRef, resize } = useCharts(props.responsive)
 
       onMounted(() => {
-        init(props.option)
+        chart.value = echarts.init(chartRef.value)
+        chart.value.setOption(props.option)
       })
 
-      onUnmounted(destroy)
-
-      return { chartRef, chart, init, resize, destroy }
+      return { chartRef, chart, resize }
     }
   })
 </script>
---
+++ src/components/DEcharts/useCharts.ts
@@ -0,0 +1,28 @@
+import type { ECharts } from 'echarts'
+import { debounce } from 'lodash-es'
+
+export function useCharts(responsive = true) {
+  const chartRef = ref<any>(null)
+  const chart = ref<ECharts | null>(null)
+
+  onMounted(() => {
+    if (responsive) {
+      window.addEventListener('resize', debounce(resize, 200))
+    }
+  })
+
+  function resize() {
+    chart.value?.resize()
+  }
+
+  function destroy() {
+    chart.value = null
+    if (responsive) {
+      window.removeEventListener('resize', resize)
+    }
+  }
+
+  onUnmounted(destroy)
+
+  return { chartRef, chart, resize, destroy }
+}
