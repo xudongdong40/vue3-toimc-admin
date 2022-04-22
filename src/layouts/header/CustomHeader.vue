4556e0589798501d9f00f245ad13b571ad7bc884
zhangying <1511455676@qq.com>
2022-04-22 12:00:21 +0800

    fix: [theme] merge CustomHeader

--- src/layouts/header/CustomHeader.vue
+++ src/layouts/header/CustomHeader.vue
@@ -23,35 +23,25 @@
     </el-row>
     <!-- Actions -->
     <el-row class="items-center flex-shrink-0">
-      <span class="items">
+      <!-- <span class="items">
         <icon type="HomeFilled" size="20px" />
       </span>
       <span class="items">
         <icon type="Bell" size="20px" />
-      </span>
-      <span id="header-internationalization" class="items">
-        <drop-down :actions="localeList" :current="getCurrent" @command="handleCommand">
-          <icon icon="ion:language" size="20px"></icon>
-        </drop-down>
-      </span>
+      </span> -->
       <span class="items" @click="handleShowThemeSetting">
         <icon collection="ri" type="brush-2-line" size="20px" />
-      </span>
-      <span class="items" @click="setStorage">
-        <el-badge :value="state.github ? 'new' : ''" class="badge">
-          <el-link :underline="false" :href="GITHUB_URL" target="_blank">
-            <icon icon="ant-design:github-filled" size="20px"></icon>
-          </el-link>
-        </el-badge>
       </span>
       <change-locale class="items"></change-locale>
       <full-screen class="items"></full-screen>
       <repo-badge class="items"></repo-badge>
+      <el-divider direction="vertical"></el-divider>
       <el-avatar
         :size="20"
         src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
       ></el-avatar>
       <span class="text-sm mr-4">管理员</span>
+      <el-divider direction="vertical"></el-divider>
       <span class="text-sm pr-1">{{ $t('Header.CustomHeader.quit') }}</span>
       <icon type="SwitchButton" size="20px" />
     </el-row>
@@ -59,12 +49,7 @@
 </template>
 
 <script lang="ts">
-  // useLocale把vue-i18n的useI18n封装了一层，加入了cache
-  import { useLocale } from '@/hooks/useLocale'
-  import { localeList } from '@/settings/localeSetting'
-  import { LocaleType } from 'types/config'
   import { useStore } from '@/store/modules/menu'
-  import { GITHUB_URL } from '@/settings/siteSetting'
   import _ from 'lodash-es'
   import { FullScreen, ChangeLocale, RepoBadge } from './components'
   export default defineComponent({
@@ -91,50 +76,28 @@
     emits: ['update:collapse', 'show-theme-setting'],
     setup(props, { emit }) {
       const { layout } = toRefs(props)
-      const { changeLocale, getLocale } = useLocale()
       const menuStore = useStore()
-      const state = useStorage('my-repo', { github: true })
-
-      const getCurrent = computed(() => {
-        return localeList.findIndex((item) => item.value + '' === getLocale.value)
-      })
-
       const allMenu = computed(() => menuStore.menus)
-
       const topMenu = computed(() => {
         return _.cloneDeep(allMenu.value)?.map((item) => {
           delete item.children
           return item
         })
       })
-
       const showMenu = computed(() => (layout.value === 'top' ? allMenu.value : topMenu.value))
 
       function handleClick(flag: boolean) {
         emit('update:collapse', !flag)
       }
 
-      function handleCommand(command: string) {
-        changeLocale(command as LocaleType)
-      }
-
       function handleShowThemeSetting() {
         emit('show-theme-setting')
-      }
-      function setStorage() {
-        state.value.github = false
       }
 
       return {
         showMenu,
         handleClick,
-        localeList,
-        handleCommand,
-        getCurrent,
-        handleShowThemeSetting,
-        GITHUB_URL,
-        state,
-        setStorage
+        handleShowThemeSetting
       }
     }
   })
