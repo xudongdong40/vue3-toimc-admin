04f40438bbb0ab7b9e5def3d68ddd5d0d2160a6f
zhangying <1511455676@qq.com>
2022-05-06 16:19:38 +0800

    fix: [theme] 缓存tabbar

--- src/store/modules/tabsbar.ts
+++ src/store/modules/tabsbar.ts
@@ -1,11 +1,18 @@
 import { AppRouteRecordRaw } from '@/router/types'
 import { defineStore } from 'pinia'
 
+const localVisitedPage = localStorage.getItem('toimc-admin-visited-page')
+
+const visitedPages = localVisitedPage !== null ? JSON.parse(localVisitedPage) : []
+
 export const useTabsStore = defineStore('tabsbar', {
   state: () => ({
-    visitedRoutes: Array<AppRouteRecordRaw>()
+    visitedRoutes: visitedPages as AppRouteRecordRaw[]
   }),
   actions: {
+    saveTabs(){
+      localStorage.setItem('toimc-admin-visited-page', JSON.stringify(this.visitedRoutes))
+    },
     addVisitedRoute(route) {
       const target = this.visitedRoutes.find((item) => item.path === route.path)
       if (target) {
@@ -13,16 +20,19 @@ export const useTabsStore = defineStore('tabsbar', {
         return
       }
       this.visitedRoutes.push(Object.assign({}, route))
+      this.saveTabs()
     },
     delVisitedRoute(route) {
       this.visitedRoutes.forEach((item, index) => {
         if (item.path === route.path) this.visitedRoutes.splice(index, 1)
       })
+      this.saveTabs()
     },
     delOthersVisitedRoute(route) {
       this.visitedRoutes = this.visitedRoutes.filter(
         (item) => item.meta.affix || item.path === route.path
       )
+      this.saveTabs()
     },
     delLeftVisitedRoute(route) {
       let index = this.visitedRoutes.length
@@ -30,6 +40,7 @@ export const useTabsStore = defineStore('tabsbar', {
         if (item.name === route.name) index = this.visitedRoutes.indexOf(item)
         return item.meta.affix || index <= this.visitedRoutes.indexOf(item)
       })
+      this.saveTabs()
     },
     delRightVisitedRoute(route) {
       let index = this.visitedRoutes.length
@@ -37,14 +48,17 @@ export const useTabsStore = defineStore('tabsbar', {
         if (item.name === route.name) index = this.visitedRoutes.indexOf(item)
         return item.meta.affix || index >= this.visitedRoutes.indexOf(item)
       })
+      this.saveTabs()
     },
     delAllVisitedRoutes() {
       this.visitedRoutes = this.visitedRoutes.filter((item) => item.meta.affix)
+      this.saveTabs()
     },
     updateVisitedRoute(route) {
       this.visitedRoutes.forEach((item) => {
         if (item.path === route.path) item = Object.assign(item, route)
       })
+      this.saveTabs()
     }
   }
 })
