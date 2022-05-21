86eaf02d085bd168f20a36bd4b6f34629b32a796
toimc <brian@toimc.com>
2022-05-21 11:25:18 +0800

    feat: 添加百度地图、高德地图

--- src/views/charts/baidu.vue
+++ src/views/charts/baidu.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>百度地图</div>
+  <div ref="wrapRef" :style="{ height, width }"></div>
 </template>
-
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue';
 
+  import { useScript } from '@/hooks/web/useScript';
+
+  const BAI_DU_MAP_URL =
+    'https://api.map.baidu.com/getscript?v=3.0&ak=GBUMGmG7VVnSmBSK7TS6py2GoPQzQmd6&services=&t=20210201100830&s=1';
   export default defineComponent({
+    name: 'BaiduMap',
+    props: {
+      width: {
+        type: String,
+        default: '100%'
+      },
+      height: {
+        type: String,
+        default: 'calc(100vh - 90px)'
+      }
+    },
     setup() {
-      return {}
-    }
-  })
-</script>
+      const wrapRef = ref<HTMLDivElement | null>(null);
+      const { toPromise } = useScript({ src: BAI_DU_MAP_URL });
 
-<style scoped></style>
+      async function initMap() {
+        await toPromise();
+        await nextTick();
+        const wrapEl = unref(wrapRef);
+        if (!wrapEl) return;
+        const BMap = (window as any).BMap;
+        const map = new BMap.Map(wrapEl);
+        const point = new BMap.Point(116.404, 39.915);
+        map.centerAndZoom(point, 15);
+        map.enableScrollWheelZoom(true);
+      }
+
+      onMounted(() => {
+        initMap();
+      });
+
+      return { wrapRef };
+    }
+  });
+</script>
