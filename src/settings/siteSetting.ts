790d7a129956ac18f53f6720fed858dc8ff7f803
toimc <brian@toimc.com>
2022-05-21 12:58:52 +0800

    refactor: 更新演示地址与文档地址

--- src/settings/siteSetting.ts
+++ src/settings/siteSetting.ts
@@ -2,7 +2,7 @@
 export const GITHUB_URL = 'https://github.com/toimc-team/vue3-toimc-admin'
 
 // vitepress-doc
-export const DOC_URL = 'https://github.com/toimc-team/vue3-toimc-admin'
+export const DOC_URL = 'https://toimc-team.github.io/vue3-toimc-admin-doc/'
 
 // site url
-export const SITE_URL = 'https://github.com/toimc-team/vue3-toimc-admin'
+export const SITE_URL = 'https://toimc-team.github.io/vue3-toimc-admin/'
