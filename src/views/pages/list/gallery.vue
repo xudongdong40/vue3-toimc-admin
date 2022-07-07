e348181182c4ab137f8dc8828a92bd9581e2f29d
toimc <brian@toimc.com>
2022-07-07 11:08:08 +0800

    style: gallery list设置定高展示

--- src/views/pages/list/gallery.vue
+++ src/views/pages/list/gallery.vue
@@ -8,7 +8,7 @@
     >
       <li v-for="file in files" :key="file.source" class="relative">
         <div
-          class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
+          class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden h-[20rem]"
         >
           <img
             :src="file.source"
